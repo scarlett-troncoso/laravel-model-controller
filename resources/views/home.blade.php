f3ac44ecb1060414af7a88b0db46174e6202d65e
scarlett-troncoso <scar.labussieri@gmail.com>
2024-04-21 18:32:04 +0200

    add tags section movies and card for print database on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Home page</h1>
+    <section id="movies">
+        <div class="container">
+            <h1>Movies</h1>
+            <!--Questo apaprirá nella parte dove abbiamo messo @yiel('content'), nell file "app.blade.php" della cartella layouts, -->
+
+            <div class="row">
+                @foreach ($movies as $movie)
+                    <div class="col-4">
+                        <div class="card">
+                            <h3>{{ $movie['title'] }}</h3>
+                            <div>{{ $movie['nationality'] }}</div>
+                            <div>Date: {{ $movie['date'] }}</div>
+                            <div>Vote: {{ $movie['vote'] }}</div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
 @endsection
