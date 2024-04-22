99c4c541fe29f501b598f17f51ec3943a88ee02a
scarlett-troncoso <scar.labussieri@gmail.com>
2024-04-22 18:12:33 +0200

    created new file contacts

---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('pageTitle', 'Contacts Page') <!--titolo per la scheda della pagina -->
+
+@section('content')
+    <h1>Contacts Page</h1>
+    <!--Questo apaprirá nella parte dove abbiamo messo @yiel('content'), nell file "app.blade.php" della cartella layouts, -->
+@endsection
