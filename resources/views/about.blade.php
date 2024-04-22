dc365e911cc3aead5d917efa0a2cf9f7f9298fc1
scarlett-troncoso <scar.labussieri@gmail.com>
2024-04-22 18:11:48 +0200

    created new file about

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('pageTitle', 'About Page') <!--titolo per la scheda della pagina -->
+
+@section('content')
+    <div class="container">
+        <h1>About Page</h1>
+        <div>
+            <img src="https://picsum.photos/200/200" alt="">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque accusantium obcaecati similique aliquam
+                quidem quia amet veniam repudiandae, quam nisi a. Nisi labore pariatur sunt officiis, explicabo eius
+                repudiandae accusamus.</p>
+        </div>
+    </div>
+@endsection
